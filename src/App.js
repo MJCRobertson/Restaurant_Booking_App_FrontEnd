0808aea89495d9ffd4ee79ca976d066395697952
Michael Alexander Robertson <MJCRobertson@outlook.com>
2019-12-17 08:02:29 +0000

    Added in the main container and passed it up to the app, now rendering hello from the main.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-// import BookinContainer from './containers/BookingContainer.js'
+import BookingContainer from './containers/BookingContainer.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <h1>Hello from the app</h1>
+      <div className="App">
+      <BookingContainer />
       </div>
     );
   }
